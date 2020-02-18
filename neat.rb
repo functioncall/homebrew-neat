9e48c86b39f4c31a627e4f63de656c7a1f5e4483
Shekhar Upadhaya <shekhar_8888@hotmail.com>
2020-02-19 01:24:01 +0530

    Added download strategy

--- neat.rb
+++ neat.rb
@@ -1,8 +1,9 @@
 class Neat < Formula
   desc "Neat is a command line utility for cleaning directories"
   homepage ""
-  url "https://github.com/functioncall/neat/archive/v1.0.0.tar.gz"
+  url "https://github.com/functioncall/neat/archive/v1.0.0.tar.gz", :using => GitDownloadStrategy
   sha256 "1cdcc50f6e54ea4960123687bb71be2181dd309761b03020b83ccc6595892750"
+  prefix.install Dir["bin/*"]
 
   def install
     bin.install "neat"
